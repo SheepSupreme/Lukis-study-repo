bfc4d9caddf668aa37723e53962675dc78c99670
lukas <61695507+SheepSupreme@users.noreply.github.com>
2024-01-11 16:03:53 +0100

    Create main.cpp

---
+++ C++/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //This is my learning c++ documentation
+
+    /*
+    Datatypes:
+    int ... 123 or -123
+    double ... 19.99 or -19.99
+    char ... 'C'  (stored in single brackets)
+    string ... "string" (stored in double brackets)
+    bool ... true or false (the same as 1 and 0)
+    */
+   string test_char = "test"; //datatype string doesn't get syntax highlight
+
+    cout << "string " << 1 << endl;
+    printf("string %d",1); // Has the same output as cout but
+    // is shorter if combining different datatypes
+
+    const int number_1 = 1; // number_1 will always be 1 (read only)
+    
+
+
+}
